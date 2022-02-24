fd30ceb361b893bf6d082046f61473de24a097d6
zheRoom <victor4309@gmail.com>
2022-02-24 11:28:36 +0800

    新增導覽列

---
+++ src/components/NavBar.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="navBar">
+    <div class="container">
+      <div class="logo">
+        <router-link to="/main">
+          <img :src="require('../assets/icon/Logo.png')" alt="" />
+        </router-link>
+      </div>
+      <div class="content">
+        <div class="homeTab">
+          <router-link class="navItem" to="/main">
+            <div class="icon">
+              <img
+                class="normalImg"
+                :src="require('../assets/icon/home.png')"
+                alt=""
+              />
+              <img
+                class="activeImg"
+                :src="require('../assets/icon/homeActive.png')"
+                alt=""
+              />
+            </div>
+            首頁
+          </router-link>
+        </div>
+        <div class="selfTab">
+          <router-link class="navItem" to="/users/self">
+            <div class="icon">
+              <img
+                class="normalImg"
+                :src="require('../assets/icon/person.png')"
+                alt=""
+              />
+              <img
+                class="activeImg"
+                :src="require('../assets/icon/personActive.png')"
+                alt=""
+              />
+            </div>
+            個人資料
+          </router-link>
+        </div>
+        <div class="settingTab">
+          <router-link class="navItem" to="/setting">
+            <div class="icon">
+              <img
+                class="normalImg"
+                :src="require('../assets/icon/setting.png')"
+                alt=""
+              />
+              <img
+                class="activeImg"
+                :src="require('../assets/icon/settingActive.png')"
+                alt=""
+              />
+            </div>
+            設定
+          </router-link>
+        </div>
+      </div>
+      <button class="btn">推文</button>
+    </div>
+    <div class="logoutTab">
+      <button class="logoutButton">
+        <div class="icon">
+        <img :src="require('../assets/icon/logout.png')" alt="" />
+        </div>登出
+      </button>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+@import "/src/scss/NavBar.scss";
+</style>
---
+++ src/views/Setting.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <div class="leftContainer"><NavBar /></div>
+    <div class="rightContainer"></div>
+  </div>
+</template>
+<script>
+import NavBar from "./../components/NavBar";
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
+<style scoped>
+.container {
+  display: grid;
+  grid-template-columns: 26% auto;
+  max-width: 1440px;
+}
+.leftContainer {
+  height: 100vh;
+}
+
+</style>

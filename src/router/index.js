282609d6f84adcc2a07249fcc081c142bdd22b05
zheRoom <victor4309@gmail.com>
2022-02-24 22:28:37 +0800

    註冊頁面&filter註解

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import SignIn from '../views/SignIn.vue'
 import NotFound from '../views/NotFound.vue'
-
+import SignUp from '../views/SignUp.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -15,6 +15,11 @@ const routes = [
     path: '/signin',
     name: 'SignIn',
     component: SignIn
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: SignUp
   },
   {
     path: '/setting',
--- src/utils/mixins.js
+++ src/utils/mixins.js
@@ -6,35 +6,35 @@ export const fromNowFilter = {
       return datetime ? moment(datetime).fromNow() : '-'
     }
   }
-}
+}//顯示推文離現在多久
 export const timeFormatFilter = {
   filters: {
     timeFormat(datetime) {
       return datetime ? moment(datetime).format('a h:mm:ss , MMMM Do YYYY') : '-'
     }
   }
-}
+}//把推文改成特定格式 ex. pm 10:25:12 , feb 24th 2022
 export const accountTagFilter = {
   filters: {
     accountTag(account) {
       return "@" + account
     }
   }
-}
+}//名字前加上@
 export const numberFormatTCFilter = {
   filters: {
     numberFormatTC(num) {
       return num.length > 4 ? `${num.substr(0, num.length - 4)}.${num.substr(num.length - 4, 1)} 萬` : num
     }
   }
-}//回傳資料為字串時適用若為數字需更改
+}//回傳資料為字串時適用若為數字需更改,數字改成XX萬
 export const numberFormatEndFilter = {
   filters: {
     numberFormatEnd(num) {
       return num.length > 3 ? `${num.substr(0, num.length - 3)}.${num.substr(num.length - 3, 1)} k` : num
     }
   }
-}//回傳資料為字串時適用若為數字需更改
+}//回傳資料為字串時適用若為數字需更改,數字改成XX千
 
 // email格式驗證，參考 https://hackmd.io/@FortesHuang/rJf6CYynS
 export const checkEmail = (val) => {

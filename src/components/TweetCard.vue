62bb58bed0beff6395c781c69552b5cc882621af
Joy-Chang-2021 <joy50422@gmail.com>
2022-02-25 00:48:38 +0800

    feat: 三種框架設定_Framework.vue、三種大頭貼格式_BasicElement.scss、新增元件TweetCard.vue、設定頁面Main.vue(未完)

---
+++ src/components/TweetCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="tweetCard-wrapper">
+    <div class="card-left">
+      <img
+        class="avatar-S" alt=""
+        :src="tweetCard.User.avatar"
+      >
+    </div>
+    <div class="card-right">
+      <div class="card-head">
+        <h5 class="card-name">{{ tweetCard.User.name }}</h5>
+        <h5 class="card-account">{{ tweetCard.User.account | accountTag }}</h5>
+        <span>●</span>
+        <h5 class="card-time">{{ tweetCard.createdAt | fromNow}}</h5>
+      </div>
+      <p>
+        {{ tweetCard.description }}
+      <p>
+      <div class="card-foot">
+        <img class="reply" alt="">
+        <h6>{{ tweetCard.Replies.length }}</h6>
+        <img class="heart" alt="">
+        <h6>{{ tweetCard.Likes.length }}</h6>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { fromNowFilter, accountTagFilter, timeFormatFilter } from '../utils/mixins'
+export default {
+  // todo: 修正 mixins 日期格式
+  mixins: [fromNowFilter, accountTagFilter, timeFormatFilter],
+  props: {
+    tweetCard: {
+      type: Object,
+      required: true,
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import SignIn from '../views/SignIn.vue'
+import Main from '../views/Main.vue'
 import NotFound from '../views/NotFound.vue'
 
 Vue.use(VueRouter)
@@ -13,8 +14,13 @@ const routes = [
   },
   {
     path: '/signin',
-    name: 'SignIn',
+    name: 'sign-in',
     component: SignIn
+  },
+  {
+    path: '/main',
+    name: 'main',
+    component: Main
   },
   {
     path: '/setting',
@@ -23,7 +29,7 @@ const routes = [
   },
   {
     path: '*',
-    name: 'Not-found',
+    name: 'not-found',
     component: NotFound
   },
 ]

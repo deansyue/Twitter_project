38bafc2fe8c3c9c7ebf61892ab5a78f26bd040a1
Joy-Chang-2021 <joy50422@gmail.com>
2022-02-26 02:52:07 +0800

    feat：UserFollowCards.vue 切版、串接 dummyData、設定 router 轉換取出 params.id

--- src/components/UserFollowTabs.vue
+++ src/components/UserFollowTabs.vue
@@ -8,13 +8,15 @@
       </div>
     </div>
     <div class="userFollowTabs-body">
-      <router-link class="userFollows-tab"
-        :to="{ name: 'users-followers', params: { id: 1}}"
+      <router-link 
+        :class="['userFollows-tab', { 'userFollows-tab-active': isFollowerPage}]"
+        :to="{ name: 'users-followers', params: { id: paramsId }}"
       >
         追隨者
       </router-link>
-      <router-link class="userFollows-tab"
-        :to="{ name: 'users-followings', params: { id: 1}}"
+      <router-link
+        :class="['userFollows-tab', { 'userFollows-tab-active': !isFollowerPage}]"
+        :to="{ name: 'users-followings', params: { id: paramsId }}"
       >
         正在跟隨
       </router-link>
@@ -22,3 +24,28 @@
     <div class="userFollows-body"></div>
   </div>
 </template>
+
+<script>
+// todo: 接收使用者名稱、推文數(userFollowTabs-title )
+// todo: 檢查 paramsId 後兩種 tab 轉換時 paramsId 仍要一致
+export default {
+  props: {
+    isFollowerPage: {
+      type: Boolean,
+      required: true
+    },
+    paramsId: {
+      type: Number,
+      required: true
+    }
+  },
+  watch: {
+    isFollowerPage(newValue) {
+      this.isFollowerPage = newValue
+    },
+    paramsId(newValue) {
+      this.paramsId = newValue
+    },
+  }
+}
+</script>
--- src/views/UserFollowers.vue
+++ src/views/UserFollowers.vue
@@ -4,7 +4,8 @@
       <NavBar />
     </div>
     <div class="middle-container">
-      <UserFollowTabs />
+      <UserFollowTabs :is-follower-page="isFollowerPage" :params-id="paramsId"/>
+      <UserFollowCards />
     </div>
     <div class="right-container">
       <Popular />
@@ -15,13 +16,31 @@
 <script>
 import NavBar from "../components/NavBar.vue";
 import UserFollowTabs from '../components/UserFollowTabs.vue'
+import UserFollowCards from '../components/UserFollowCards.vue'
 import Popular from "../components/Popular.vue";
 
+// todo: 串接 API 取得資料
 export default {
   components: {
     NavBar,
     UserFollowTabs,
+    UserFollowCards,
     Popular,
   },
+  data() {
+    return {
+      isFollowerPage: true,
+      paramsId: 0,
+    }
+  },
+  created() {
+    const { id } = this.$route.params
+    this.paramsId = Number(id)
+  },
+  beforeRouteUpdate (to, from, next) {
+    const { id } = to.params
+    this.paramsId = Number(id)
+    next()
+  }
 };
 </script>

ee75582cb1bbd1aa9ea58e9d20741b7459e02ca9
Joy-Chang-2021 <joy50422@gmail.com>
2022-02-25 18:31:49 +0800

    feat：（回覆卡片）資料初步處理

--- src/components/ReplyCard.vue
+++ src/components/ReplyCard.vue
@@ -2,7 +2,7 @@
   <div class="replyCard-wrapper">
     <div class="card-left">
       <router-link :to="{ name: 'user', params: { id: userId }}">
-        <img class="avatar" src="https://loremflickr.com/g/320/240/people/?random=91.66143782652539">
+        <img class="avatar" :src="image">
       </router-link>
     </div>
     <div class="card-right">
@@ -10,10 +10,10 @@
         <router-link class="card-name"
           :to="{ name: 'user', params: { id: userId }}"
         >
-          name
+          {{ userName }}
         </router-link>
         <h5 class="card-account">
-          account
+          {{ userAccount | accountTag }}
         </h5>
         <span>．</span>
         <h5 class="card-time">
@@ -23,42 +23,68 @@
       <h5>回覆
         <router-link
           class="card-replyer"
-          :to="{ name: 'user', params: { id: userId }}"
+          :to="{ name: 'user', params: { id: targetUserId }}"
         >
-          {{ name | accountTag }}
+          {{ targetAccount | accountTag }}
         </router-link>
       </h5>
-      <p>亂碼碼</p>
+      <p>{{ comment }}</p>
     </div>
   </div>
 </template>
 
 <script>
 import { fromNowFilter, accountTagFilter } from '../utils/mixins'
+
+// 假設傳入的資料
+const dummyData = {
+  // 被回覆的對象
+  targetAccount: '被回覆的對象',
+  targetUserId: 3,
+  // 此卡片的其他資料
+  tweetId: 13,
+  userId: 4,
+  comment: 'Maiores totam nobis corrupti dolores qui et est consectetur hic. Nec ess ita tibus rem quidem blanditiis iusto reiciendis ipsam id archi tecto.',
+  createdAt: '2022-02-24T08:19:31.000Z',
+  userName: '此卡片user',
+  userAccount: '此卡片account',
+  image: 'https://loremflickr.com/g/320/240/people/?random=91.66143782652539'
+}
+
 export default {
   mixins: [fromNowFilter, accountTagFilter],
-  props: {
-    replyCard: {
-      type: Object,
-      required: true,
-    }
-  },
+  // props: {
+  //   replyCard: {
+  //     type: Object,
+  //     required: true,
+  //   }
+  // },
   data() {
     return {
-      id: 0,
+      // 被回覆的對象
+      targetAccount: '',
+      targetUserId: 0,
+      // 此卡片的其他資料
+      tweetId: 0,
       userId: 0,
       comment: '',
       createdAt: '',
-      name: 'apple' // waiting check
+      userName: '',
+      userAccount: '',
+      image: ''
     }
   },
   methods: {
     fetchReplyCard() {
-      const { id, UserId, comment, createdAt } = this.replyCard
-      this.id = id
-      this.userId = UserId
-      this.comment = comment
-      this.createdAt = createdAt
+      this.targetAccount = dummyData.targetAccount
+      this.targetUserId = dummyData.targetUserId
+      this.tweetId = dummyData.tweetId
+      this.userId = dummyData.userId
+      this.comment = dummyData.comment
+      this.createdAt = dummyData.createdAt
+      this.userName = dummyData.userName
+      this.userAccount = dummyData.userAccount
+      this.image = dummyData.image
     },
   },
   created() {
--- src/views/Tweet.vue
+++ src/views/Tweet.vue
@@ -5,7 +5,10 @@
     </div>
     <div class="middle-container">
       <div v-for="replyCard in replyCards" :key="replyCard.id">
-        <ReplyCard :reply-card="replyCard"/>
+        <ReplyCard 
+          :relyer="replyer"
+          :reply-card="replyCard"
+        />
       </div>
     </div>
     <div class="right-container">
@@ -18,53 +21,111 @@
 import NavBar from '../components/NavBar.vue'
 import ReplyCard from '../components/ReplyCard.vue'
 
-const dummyData = {
-  "id": 1,
-  "UserId": 2,
-  "TweetId": 1,
-  "comment": "Maiores totam nobis corrupti dolores qui et est consectetur hic. Necessitatibus rem quidem blanditiis iusto reiciendis ipsam id architecto. ",
-  "createdAt": "2022-02-24T08:19:31.000Z",
-  "updatedAt": "2022-02-24T08:19:31.000Z",
-  "User": {
-    "id": 2,
-    "account": "user1",
-    "email": "user1@example.com",
-    "password": "$2a$10$Is0KM9XOstEfEjeTqskiBeCCz1UURsKxNrPCCMCMb9G8eCqIB1Seu",
-    "name": "user1",
-    "avatar": "https://loremflickr.com/g/320/240/people/?random=91.66143782652539",
-    "cover": "https://loremflickr.com/g/600/240/shop/?random=30.68038263159545",
-    "introduction": "Culpa minus ea ullam.",
-    "role": "user",
-    "createdAt": "2022-02-24T08:19:30.000Z",
-    "updatedAt": "2022-02-24T08:19:30.000Z"
+// 單篇推文的回覆內容：
+// const dataOne
+
+// 個人主頁的回覆內容：
+const dummyDataTwo = {
+  User: {
+    id: 2,
+    account: "user1",
+    email: "user1@example.com",
+    password: "$2a$10$Is0KM9XOstEfEjeTqskiBeCCz1UURsKxNrPCCMCMb9G8eCqIB1Seu",
+    name: "user1",
+    avatar:
+      "https://loremflickr.com/g/320/240/people/?random=91.66143782652539",
+    cover: "https://loremflickr.com/g/600/240/shop/?random=30.68038263159545",
+    introduction: "Culpa minus ea ullam.",
+    role: "user",
+    createdAt: "2022-02-24T08:19:30.000Z",
+    updatedAt: "2022-02-24T08:19:30.000Z"
   },
-  "Replys": [
+  Replies: [
     {
-      "id": 3,
-      "UserId": 4,
-      "TweetId": 1,
-      "comment": "Quasi modi sint fuga recusandae. Et et ducimus consectetur soluta. Deleniti inventore error possimus nesciunt itaque ut sunt.",
-      "createdAt": "2022-02-24T08:19:31.000Z",
-      "updatedAt": "2022-02-24T08:19:31.000Z"
+      id: 1,
+      UserId: 2,
+      TweetId: 1,
+      comment:
+        "Maiores totam nobis corrupti dolores qui et est consectetur hic. Necessitatibus rem quidem blanditiis iusto reiciendis ipsam id architecto. ",
+      createdAt: "2022-02-24T08:19:31.000Z",
+      updatedAt: "2022-02-24T08:19:31.000Z",
+      Tweet: {
+        id: 1,
+        UserId: 2,
+        description: "a",
+        createdAt: "2022-02-24T08:19:31.000Z",
+        updatedAt: "2022-02-24T08:19:31.000Z",
+        User: {
+          id: 2,
+          name: "user1",
+          account: "user1"
+        }
+      }
     },
     {
-      "id": 2,
-      "UserId": 2,
-      "TweetId": 1,
-      "comment": "odit",
-      "createdAt": "2022-02-24T08:19:31.000Z",
-      "updatedAt": "2022-02-24T08:19:31.000Z"
+      id: 2,
+      UserId: 2,
+      TweetId: 1,
+      comment: "odit",
+      createdAt: "2022-02-24T08:19:31.000Z",
+      updatedAt: "2022-02-24T08:19:31.000Z",
+      Tweet: {
+        id: 1,
+        UserId: 2,
+        description: "a",
+        createdAt: "2022-02-24T08:19:31.000Z",
+        updatedAt: "2022-02-24T08:19:31.000Z",
+        User: {
+          id: 2,
+          name: "user1",
+          account: "user1"
+        }
+      }
     },
     {
-      "id": 1,
-      "UserId": 2,
-      "TweetId": 1,
-      "comment": "Maiores totam nobis corrupti dolores qui et est consectetur hic. Necessitatibus rem quidem blanditiis iusto reiciendis ipsam id architecto. ",
-      "createdAt": "2022-02-24T08:19:31.000Z",
-      "updatedAt": "2022-02-24T08:19:31.000Z"
+      id: 3,
+      UserId: 4,
+      TweetId: 1,
+      comment:
+        "Quasi modi sint fuga recusandae. Et et ducimus consectetur soluta. Deleniti inventore error possimus nesciunt itaque ut sunt.",
+      createdAt: "2022-02-24T08:19:31.000Z",
+      updatedAt: "2022-02-24T08:19:31.000Z",
+      Tweet: {
+        id: 1,
+        UserId: 2,
+        description: "a",
+        createdAt: "2022-02-24T08:19:31.000Z",
+        updatedAt: "2022-02-24T08:19:31.000Z",
+        User: {
+          id: 2,
+          name: "user1",
+          account: "user1"
+        }
+      }
+    },
+    {
+      id: 4,
+      UserId: 2,
+      TweetId: 2,
+      comment: "Nulla est veniam.\nEius nihil ex.",
+      createdAt: "2022-02-24T08:19:31.000Z",
+      updatedAt: "2022-02-24T08:19:31.000Z",
+      Tweet: {
+        id: 2,
+        UserId: 2,
+        description:
+          "Ipsam expedita natus accusamus eos expedita doloribus qui veniam est. Mollitia est eos et ratione eaque distinctio. Eum magnam dolor. Odio nobis minus at cum illum et ex dignissimos.",
+        createdAt: "2022-02-24T08:19:31.000Z",
+        updatedAt: "2022-02-24T08:19:31.000Z",
+        User: {
+          id: 2,
+          name: "user1",
+          account: "user1"
+        }
+      }
     }
-  ],
-}
+  ]
+};
 
 export default {
   components: {
@@ -74,17 +135,20 @@ export default {
   data () {
     return {
       // todo: 本頁的主推文資料＆切版
+      replyer: {},
       replyCards: []
     }
   },
   methods: {
-    fetchReplyCards () {
+    // 個人主頁：
+    fetchOtherReplyCards () {
       // todo: 串接 API
-      this.replyCards = dummyData.Replys
+      this.replyer = { ...dummyDataTwo.User }
+      this.replyCards = [ ...dummyDataTwo.Replies ]
     }
   },
   created () {
-    this.fetchReplyCards ()
+    this.fetchOtherReplyCards ()
   }
 }
 </script>

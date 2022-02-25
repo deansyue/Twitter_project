63dc7623a87dd0d609a478afe91c1b1f54927ae5
zheRoom <victor4309@gmail.com>
2022-02-25 15:46:50 +0800

    換用共同框架

--- src/views/UserSelf.vue
+++ src/views/UserSelf.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="userSelf-wrapper">
+  <div class="app-tripple-column">
     <div class="left-container"><NavBar /></div>
     <div class="middle-container"></div>
     <div class="right-container"><Popular /></div>
@@ -11,20 +11,7 @@ import Popular from "./../components/Popular";
 export default {
   components: {
     NavBar,
-    Popular
+    Popular,
   },
-}
+};
 </script>
-<style lang="scss">//要放popular先畫框架最後刪掉
-.userSelf-wrapper {
-  max-width: 1440px;
-  display: grid;
-  grid-template-columns: 26% 42% 32%;
-  margin: 0 auto;
-  .middle-container {
-  border-left: 1px solid #e6ecf0;
-  border-right: 1px solid #e6ecf0;
-  height: 100vh;
-  }
-} 
-</style>

6b202ea779a76132f1920b052b688dcf20d20749
Joy-Chang-2021 <joy50422@gmail.com>
2022-02-27 05:56:06 +0800

    feat：主頁、瀏覽貼文頁面，初步測試 API 資料

--- src/apis/authorization.js
+++ src/apis/authorization.js
@@ -1,10 +1,10 @@
 import { apiHelper } from '../utils/helpers'
 
 export default {
-  SignIn({account, password}) {
+  SignIn ({account, password}) {
     return apiHelper.post('api/users/signin', {
       account,
       password
     })
-  }
+  },
 }

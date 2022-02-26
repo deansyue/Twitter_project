e2ce00eaf61a6276309fc576c30e2787d2d975a7
zheRoom <victor4309@gmail.com>
2022-02-26 15:52:36 +0800

    新增usercard元件

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -9,35 +9,102 @@
         <div class="tweetCount">{{ tweets.length }}推文</div>
       </div>
     </div>
-    <div class="user-wrapper"></div>
+    <div class="user-wrapper">
+      <img :src="userData.cover" alt="" class="cover" />
+      <img :src="userData.avatar" alt="" class="avatar-big" />
+      <button class="btn btn-edit" v-if="whichPage">編輯個人資料</button>
+      <div class="otherButton" v-else>
+        <img src="" alt="" class="message" />
+        <img
+          src=""
+          alt=""
+          class="notice"
+          v-if="isNotice"
+          @click="isNotice = false"
+        />
+        <img
+          src=""
+          alt=""
+          class="notice-active"
+          v-else
+          @click="isNotice = true"
+        />
+        <button
+          class="btn btnDeleteFollow"
+          v-if="isFollowed"
+          @click="isFollowed = false"
+        >
+          正在追隨
+        </button>
+        <button class="btn btnAddFollow" v-else @click="isFollowed = true">
+          追隨
+        </button>
+      </div>
+      <div class="information-wrapper">
+        <div class="name">{{ userData.name }}</div>
+        <div class="account">{{ userData.account | accountTag }}</div>
+        <div class="introduce">{{ userData.introduction }}</div>
+        <div class="follow">
+          <router-link
+            :to="{ name: 'users-followings', params: { id: userData.id } }"
+            class="strong"
+          >
+            {{ userData.followingCount | numberFormatTC}}個
+          </router-link>
+          追隨中<router-link
+            :to="{ name: 'users-followers', params: { id: userData.id } }"
+            class="strong2"
+            >{{ userData.follwerCount | numberFormatTC}}位</router-link
+          >追隨者
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import { accountTagFilter } from "./../utils/mixins";
+import { numberFormatTCFilter } from "./../utils/mixins";
 export default {
+  mixins: [accountTagFilter,numberFormatTCFilter],
   props: {
-    currentUser: {
+    currentUserData: {
       type: Object,
       default: () => ({
-        id: "",
+        id: -1,
         account: "",
         name: "",
         email: "",
-        password: "",
+        avatar: "",
+        cover: "",
+        introduction: "",
+        followingCount: -1,
+        follwerCount: -1,
       }),
     },
     tweets: {
       type: Array,
       require: true,
     },
+    whichPage: {
+      type: Boolean,
+      require: true,
+    },
   },
   data() {
     return {
       userData: {
-        id: "",
+        id: -1,
         account: "",
         name: "",
         email: "",
+        avatar: "",
+        cover: "",
+        introduction: "",
+        followingCount: -1,
+        follwerCount: -1,
       },
+      isFollowed: true,
+      isNotice: true,
     };
   },
   created() {
@@ -45,10 +112,15 @@ export default {
   },
   methods: {
     fetchUser() {
-      this.userData.id = this.currentUser.id;
-      this.userData.account = this.currentUser.account;
-      this.userData.name = this.currentUser.name;
-      this.userData.email = this.currentUser.email;
+      this.userData.id = this.currentUserData.id;
+      this.userData.account = this.currentUserData.account;
+      this.userData.name = this.currentUserData.name;
+      this.userData.email = this.currentUserData.email;
+      this.userData.avatar = this.currentUserData.avatar;
+      this.userData.cover = this.currentUserData.cover;
+      this.userData.introduction = this.currentUserData.introduction;
+      this.userData.followingCount = this.currentUserData.followingCount;
+      this.userData.follwerCount = this.currentUserData.follwerCount;
     },
   },
 };
--- src/store/index.js
+++ src/store/index.js
@@ -10,7 +10,10 @@ export default new Vuex.Store({
       account: "heyJohn",
       name: "John Doe",
       email: "John123@gmail.com",
-      password: "123",
+      avatar:
+        "https://loremflickr.com/g/320/240/people/?random=91.66143782652539",
+      cover: "https://loremflickr.com/g/600/240/shop/?random=30.68038263159545",
+      introduction: "Culpa minus ea ullam.",
     },
   },
   mutations: {
